be9524ef138207744d366d82e02b0b82283bdccb
FFFF <524182647@qq.com>
2019-07-01 15:17:13 +0800

    news

--- public/javascripts/custom-global.js
+++ public/javascripts/custom-global.js
@@ -7,7 +7,7 @@
  * @constructor
  */
 $.fn.Sidebar = function (options) {
-
+    
     var $this = this;
     
     /**
@@ -17,7 +17,8 @@ $.fn.Sidebar = function (options) {
      */
     function _open(obj) {
         $this.addClass('open');
-        $(obj).data("status","open");
+        $this.attr("data-status", "open");
+        $(obj).data("status", "open");
     }
     
     /**
@@ -27,8 +28,9 @@ $.fn.Sidebar = function (options) {
      */
     function _close(obj) {
         $this.removeClass('open');
+        $this.attr("data-status", "close");
         $(obj).data("status", "close");
-    
+        
     }
     
     this.open = function (obj) {
@@ -42,3 +44,53 @@ $.fn.Sidebar = function (options) {
     
     return this;
 };
+
+/**
+ * 获取 Codemirror 插件的 option 配置选项
+ * @returns {{mode: {name: string, globalVars: boolean}, foldGutter: boolean, lineNumbers: boolean, smartIndent: boolean, indentUnit: number, autoCloseBrackets: boolean, extraKeys: {"Ctrl-Space": string}, gutters: string[], theme: string, matchBrackets: boolean, autoCloseTags: boolean, lineWrapping: boolean}}
+ */
+function getCodemirrorConfig() {
+    return {
+        mode: {name: "htmlmixed", globalVars: true},
+        lineNumbers: true,
+        lineWrapping: true,
+        smartIndent: true,
+        indentUnit: 4,
+        foldGutter: true,
+        extraKeys: {"Ctrl-Space": "autocomplete"},
+        gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
+        matchBrackets: true, // 括号匹配
+        autoCloseBrackets: true,
+        autoCloseTags: true,
+        theme: "material",
+    };
+}
+
+/**
+ * 初始化 Codemirror 编辑器插件
+ * @param id{number} 需要初始化的文本域元素ID
+ */
+function initCodemirror(id) {
+    CodeMirror.fromTextArea(document.getElementById(id), getCodemirrorConfig());
+}
+
+/**
+ * 初始化自定义滚动条
+ * @param element{string} 需要添加滚动条的元素ID或元素class
+ */
+function initScrollbar(element) {
+    var ids = element || "";
+    var Scrollbar;
+    if (ids == "body") {
+        Scrollbar = new GeminiScrollbar({
+            element: document.body
+        }).create();
+    } else {
+        Scrollbar = new GeminiScrollbar({
+            element: document.querySelector(element),
+        }).create();
+    }
+    return Scrollbar;
+}
+
+
